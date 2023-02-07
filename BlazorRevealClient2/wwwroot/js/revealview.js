e0d45352b30666e489d6d2371ecf911077c0194e
yoshi1220 <19341602+yoshi1220@users.noreply.github.com>
2023-02-07 17:11:34 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorRevealClient2/wwwroot/js/revealview.js
@@ -0,0 +1,10 @@
+﻿window.loadRevealView = function (viewId, dashboardName) {
+    $.ig.RevealSdkSettings.ensureFontsLoadedAsync().then(
+
+        $.ig.RVDashboard.loadDashboard(dashboardName, (dashboard) => {
+            console.log(dashboard);
+            var revealView = new $.ig.RevealView("#" + viewId);
+            revealView.dashboard = dashboard;
+        })
+    );
+}
